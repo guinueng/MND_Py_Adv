a3baddc54b87ed7157997b04d787ea90381ac47f
guinueng <admin@guinueng.com>
2023-05-19 14:59:52 +0000

    Django Course 18 / fetch dv data and apply for loop imperfect

--- Guinueng/accountapp/views.py
+++ Guinueng/accountapp/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 from accountapp.models import Hello_World
 
 # Create your views here.
 
 def hello_world(request):
-	if request.method == "POST":
-		temp = request.POST.get('hello_world_input')
-		new_hello_world = Hello_World()
-		new_hello_world.text = temp
-		new_hello_world.save()
-		return render(request, 'accountapp/hello_world.html', context = {'hello_world_output': new_hello_world})
-	else:
-		return render(request, 'accountapp/hello_world.html', context = {'text' : 'GET METHOD!!!'})
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        
+        new_hello_world = Hello_World()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        
+        hello_world_list = Hello_World.objects.all()
+        
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = Hello_World.objects.all()
+        return render(request, 'accountapp/hello_world.html', context = {'text' : 'GET METHOD!!!'})
