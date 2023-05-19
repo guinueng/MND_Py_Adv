a77f35b48d42f219e06a389fc77d53bcf6825715
guinueng <admin@guinueng.com>
2023-05-19 23:02:12 +0000

    Django Course 18 / Solved Prob. Yesterday

--- Guinueng/accountapp/views.py
+++ Guinueng/accountapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from accountapp.models import Hello_World
 
@@ -8,14 +9,13 @@ from accountapp.models import Hello_World
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')
-        
+
         new_hello_world = Hello_World()
         new_hello_world.text = temp
         new_hello_world.save()
-        
+
         hello_world_list = Hello_World.objects.all()
-        
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = Hello_World.objects.all()
-        return render(request, 'accountapp/hello_world.html', context = {'text' : 'GET METHOD!!!'})
+        return render(request, 'accountapp/hello_world.html', context = {'hello_world_list' : hello_world_list})
