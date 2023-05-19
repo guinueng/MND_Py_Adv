cfc4132cc2922dc51de32ab434059953cb762142
guinueng <admin@guinueng.com>
2023-05-19 23:59:33 +0000

    Django course 21 / add account_create_view

--- Guinueng/accountapp/urls.py
+++ Guinueng/accountapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from accountapp.views import hello_world
+from accountapp.views import hello_world, Account_Create_View
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
-    ]
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', Account_Create_View.as_view(), name = 'create'),
+]
--- Guinueng/accountapp/views.py
+++ Guinueng/accountapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from accountapp.models import Hello_World
 
 # Create your views here.
@@ -14,8 +16,13 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = Hello_World.objects.all()
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = Hello_World.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return render(request, 'accountapp/hello_world.html', context = {'hello_world_list' : hello_world_list})
+
+class Account_Create_View(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
