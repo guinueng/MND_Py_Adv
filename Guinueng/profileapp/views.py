85a2c6ccfbba3362c848690f5d8914da50cf9c9f
guinueng <admin@guinueng.com>
2023-05-20 14:33:32 +0000

    Django Course 32 | Finish making Profileapp

--- Guinueng/profileapp/views.py
+++ Guinueng/profileapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
+from django.utils.decorators import method_decorator
 from profileapp.models import Profile
 from profileapp.forms import Profile_Creation_Form
+from profileapp.decorators import profile_ownership_required
 # Create your views here.
 
 class Profile_Create_View(CreateView):
@@ -17,3 +19,13 @@ class Profile_Create_View(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class Profile_Update_View(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = Profile_Creation_Form
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
