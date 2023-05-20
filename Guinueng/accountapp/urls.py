dfcc03ba1c2119f3b7bb2cde2d4ca0ed6791caab
guinueng <admin@guinueng.com>
2023-05-20 06:30:13 +0000

    Django Course 24 | Making personal page by using DetailView

--- Guinueng/accountapp/urls.py
+++ Guinueng/accountapp/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accountapp.views import hello_world, Account_Create_View
+from accountapp.views import hello_world, Account_Create_View, Account_Detail_View
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
-    
-    path('login/', LoginView.as_view(template_name = 'accountapp/login.html'), name='login'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    
-    path('create/', Account_Create_View.as_view(), name = 'create'),
+
+    path('create/', Account_Create_View.as_view(), name='create'),
+    path('detail/<int:pk>', Account_Detail_View.as_view(), name='detail'),
 ]
--- Guinueng/accountapp/views.py
+++ Guinueng/accountapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from accountapp.models import Hello_World
@@ -9,20 +9,25 @@ from accountapp.models import Hello_World
 # Create your views here.
 
 def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
+    if request.method=="POST":
+        temp=request.POST.get('hello_world_input')
 
-        new_hello_world = Hello_World()
-        new_hello_world.text = temp
+        new_hello_world=Hello_World()
+        new_hello_world.text=temp
         new_hello_world.save()
 
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = Hello_World.objects.all()
-        return render(request, 'accountapp/hello_world.html', context = {'hello_world_list' : hello_world_list})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
 
 class Account_Create_View(CreateView):
-    model = User
-    form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/create.html'
+    model=User
+    form_class=UserCreationForm
+    success_url=reverse_lazy('accountapp:hello_world')
+    template_name='accountapp/create.html'
+
+class Account_Detail_View(DetailView):
+    model=User
+    context_object_name='target_user'
+    template_name='accountapp/detail.html'
