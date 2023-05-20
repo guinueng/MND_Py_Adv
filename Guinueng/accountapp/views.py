e7d9c8add18cd335c5250d0cd80fa16c4ad039dc
guinueng <admin@guinueng.com>
2023-05-20 13:02:32 +0000

    Django Course 28 | Thinner code by using Decorator

--- Guinueng/accountapp/views.py
+++ Guinueng/accountapp/views.py
@@ -4,29 +4,31 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from accountapp.models import Hello_World
 from accountapp.forms import Account_Update_Form
+from accountapp.decorators import account_ownership_required
 
 # Create your views here.
+has_ownership = [account_ownership_required, login_required]
 
+
+@login_required
 def hello_world(request):
 
-    if request.user.is_authenticated:
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
 
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
+        new_hello_world = Hello_World()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            new_hello_world = Hello_World()
-            new_hello_world.text = temp
-            new_hello_world.save()
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = Hello_World.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = Hello_World.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
 
 class Account_Create_View(CreateView):
     model = User
@@ -34,10 +36,14 @@ class Account_Create_View(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class Account_Detail_View(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
 
 class Account_Update_View(UpdateView):
     model = User
@@ -46,6 +52,9 @@ class Account_Update_View(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class Account_Delete_View(DeleteView):
     model = User
     context_object_name = 'target_user'
