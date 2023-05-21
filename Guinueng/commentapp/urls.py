07d64985486c3421bb37a922ed78f933c8967bf1
guinueng <admin@guinueng.com>
2023-05-21 07:23:08 +0000

    Django Course 38 | Completing Commentapp

--- Guinueng/commentapp/urls.py
+++ Guinueng/commentapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from commentapp.views import Comment_Create_View
+from commentapp.views import Comment_Create_View, Comment_Delete_View
 app_name = 'commentapp'
 
 urlpatterns = [
     path('create/', Comment_Create_View.as_view(), name='create'),
+    path('delete/<int:pk>', Comment_Delete_View.as_view(), name='delete'),
 
 ]
